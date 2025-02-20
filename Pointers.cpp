6abf06f08b676f5b81c8a3574be8f39edb134ed6
skateing25 <145472990+skateing25@users.noreply.github.com>
2025-02-20 16:08:53 +0000

    work on pointer

---
+++ Pointers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class cool{
+public:
+string food; 
+  string* ptr;
+};
+int main() {
+  
+cool u;
+ u.food="i";
+ u.ptr= = &food;
+  cout << food << "\n";
+
+  cout << &food << "\n";
+
+  cout << ptr << "\n";
+  return 0;
+}
